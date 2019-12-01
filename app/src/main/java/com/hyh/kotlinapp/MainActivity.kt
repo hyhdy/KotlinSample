52574c5b5cbf9cad969fdc3e0e8315f44d7f1296
hyh <fjnyhyh@163.com>
2019-12-01 12:55:17 +0800

    kotlin冒泡排序实现

--- app/src/main/java/com/hyh/kotlinapp/MainActivity.kt
+++ app/src/main/java/com/hyh/kotlinapp/MainActivity.kt
@@ -2,11 +2,32 @@ package com.hyh.kotlinapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.hyh.kotlinapp.sort.BubbleSort
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //实例化对象
+        val bubble = BubbleSort()
+
+        //隐式声明一个整型数组
+        val datas = intArrayOf(5,4,3,2,1)
+        bubble.sort(datas)
+        //遍历数组的值
+        for(data:Int in datas){
+            Log.d("hyh", "data="+data);
+        }
+
+        //显示声明一个整型数组
+        val datas2:Array<Int> = arrayOf(15,42,378,2,1)
+        bubble.sort1(datas2)
+        //通过索引遍历数组
+        for(i in datas2.indices){
+            Log.d("hyh", "data="+datas2[i]);
+        }
     }
 }
--- app/src/main/java/com/hyh/kotlinapp/sort/BubbleSort.kt
+++ app/src/main/java/com/hyh/kotlinapp/sort/BubbleSort.kt
@@ -1,7 +1,50 @@
 package com.hyh.kotlinapp.sort
 
+import android.util.Log
+
 /**
  * created by curdyhuang on 2019-11-06
  */
 class BubbleSort {
+
+    /**
+     * IntArray表示整形数组类型
+     */
+    fun sort(datas:IntArray){
+        //val相当于java中的final，赋值后就不能再该变值
+        val index = datas.size-1
+
+        //根据下标遍历数组
+        for(i in 0..index) {
+            for (j in 0..index-1-i) {
+                if(datas[j]>datas[j+1]){
+                    val tmp = datas[j];
+                    datas[j] = datas[j+1];
+                    datas[j+1] = tmp;
+                }
+            }
+        }
+    }
+
+    /**
+     * 返回值Unit相当于java的void，可以直接省略不写
+     * Array<Int>表示整型数组
+     */
+    fun sort1(datas:Array<Int>):Unit{
+        //var赋值后可以再改变值，隐式声明变量类型
+        var size = datas.size;
+        //显示声明变量类型
+        var size2:Int = datas.size;
+
+        //根据下标遍历数组
+        for(i in 0..size-1) {
+            for (j in 0..size-2-i) {
+                if(datas[j]>datas[j+1]){
+                    val tmp = datas[j];
+                    datas[j] = datas[j+1];
+                    datas[j+1] = tmp;
+                }
+            }
+        }
+    }
 }
